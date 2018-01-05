3878974888e815721f38025e781b9d4713dee9bf
leonvdw <lvanderwalt@retrorabbit.co.za>
2018-01-05 15:32:14 +0200

    Finished left side

--- src/accountScreen/accountScreen.js
+++ src/accountScreen/accountScreen.js
@@ -7,17 +7,34 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 
 
 const ChatCard = function(props) {
-  return (
-    <Card>
-      <CardHeader
-        title={props.title}
-        avatar={props.avatar}
-      />
-      <CardText>
-        {props.text}
-      </CardText>
-    </Card>
+  if(props.selected){
+    return (
+      <Card style={{
+        backgroundColor:'#cacaca'}}>
+        <CardHeader
+          title={props.title}
+          avatar={props.avatar}
+        />
+        <CardText>
+          {props.text}
+        </CardText>
+      </Card>        
     );
+  }
+
+  else{
+    return (
+      <Card>
+        <CardHeader
+          title={props.title}
+          avatar={props.avatar}/>
+        <CardText>
+          {props.text}
+        </CardText>
+      </Card>        
+    );
+  }
+  
 };
 
 class AccountScreen extends Component {
@@ -36,27 +53,28 @@ class AccountScreen extends Component {
             <ChatCard title="Lloyd Jimenez"
             avatar="http://cliparting.com/wp-content/uploads/2016/10/Person-people-icon-clipart-kid.png"
             text="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s."
+            selected={true}
             />
           </div>
 
           <div className='cardContainer'>
-            <ChatCard title="Lloyd Jimenez"
+            <ChatCard title="Jeffrey Thomas"
             avatar="http://cliparting.com/wp-content/uploads/2016/10/Person-people-icon-clipart-kid.png"
-            text="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s."
+            text="When you type the website name on your address bar, a simple yet classy homepag"
             />
           </div>
 
           <div className='cardContainer'>
-            <ChatCard title="Lloyd Jimenez"
+            <ChatCard title="Catherine Sanders"
             avatar="http://cliparting.com/wp-content/uploads/2016/10/Person-people-icon-clipart-kid.png"
-            text="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s."
+            text="It is not always possible to jet off half way around the world when you and your significant other"
             />
           </div>
 
           <div className='cardContainer'>
-            <ChatCard title="Lloyd Jimenez"
+            <ChatCard title="Terry Gordon"
             avatar="http://cliparting.com/wp-content/uploads/2016/10/Person-people-icon-clipart-kid.png"
-            text="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s."
+            text="Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola"
             />
           </div>
 
