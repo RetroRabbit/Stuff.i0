ac0f52ccf0d95ffc3ba4080c340a73056d52d153
Joseph <jsirwali@retrorabbit.co.za>
2018-01-05 11:28:03 +0200

    Redux setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom'
+import './css/index.css';
+import { headerNav } from './header.js'
+
+// TODO : Import all the components
+import home from './components/home'
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <headerNav></headerNav>
+          <Link to="/">Home</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main>
+          <Route exact path="/" component={home} />
+        </main>
       </div>
     );
   }
---
+++ src/components/headerNav.js
@@ -0,0 +1,15 @@
+
+import React,{ Component } from 'react'
+
+class headerNav extends Component{
+  render(){
+    return (
+      <div>
+        <h1>This is the header</h1>
+        <p>Hello Medium!</p>
+      </div>
+    )
+  }
+}
+
+export default headerNav;
