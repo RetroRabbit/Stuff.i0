972295e8542d70f612f09cfb9e538b1f427ad820
Alicia <ahlatshwayo@retrorabbit.co.za>
2018-01-05 13:01:55 +0200

    Registration page

--- src/registration/index.js
+++ src/registration/index.js
@@ -6,51 +6,54 @@ import RaisedButton from 'material-ui/RaisedButton';
 import './reg.css'
 
 class Register extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-    name:'',
-    email:'',
-    password:''
-    }
-   }
-  render() {
-      return (
-        <div>
-          <MuiThemeProvider>
-            <div className="rectangle">
-              <h2 className="step-one">Step One</h2>
-              <h2 className="the-basics">THE BASICS</h2>
-            <div className="form-field">
-             <TextField
-               hintText="Enter your Name"
-               floatingLabelText="Your Name"
-               onChange = {(event,newValue) => this.setState({username:newValue})}
-               />
-             <br/>
-               <TextField
-                 hintText="Enter your Email"
-                 floatingLabelText="Email"
-                 onChange = {(event,newValue) => this.setState({password:newValue})}
-                 />
-              <br/>
-                 <TextField
-                 type="password"
-                 hintText="Enter your Password"
-                 floatingLabelText="Password"
-                 onChange = {(event,newValue) => this.setState({password:newValue})}
-                 />
-               <br/>
-               <RaisedButton label="Next Step" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-           </div>
-              </div>
-           </MuiThemeProvider>
-        </div>
-      );
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
     }
   }
-  const style = {
-   margin: 15,
-  };
-  export default Register;
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div className="space">
+
+            </div>
+          <div className="rectangle">
+            <h2 className="step-one">Step One</h2>
+            <h2 className="the-basics">THE BASICS</h2>
+            <div className="form-field">
+              <TextField
+                hintText="Enter your Name"
+                floatingLabelText="Your Name"
+                onChange={(event, newValue) => this.setState({ username: newValue })}
+              />
+              <br />
+              <TextField
+                hintText="Enter your Email"
+                floatingLabelText="Email"
+                onChange={(event, newValue) => this.setState({ password: newValue })}
+              />
+              <br />
+              <TextField
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange={(event, newValue) => this.setState({ password: newValue })}
+              />
+              <br />
+              <RaisedButton label="Next Step" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
+            </div>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
+export default Register;
 
