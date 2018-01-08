2b635517f3ca8557f4df1fc61918a57e5ee47e81
Samantha Harris <sharris@retrorabbit.co.za>
2018-01-08 14:30:31 +0200

    registration step two - styling

--- src/registration/StepTwo.js
+++ src/registration/StepTwo.js
@@ -1,86 +1,83 @@
-import React from 'react'
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import './StepTwo.css'
+import './StepTwo.css';
 
 class Register2 extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
-  state = {
-    profilephoto: '',
-    hasimg: false,
-  }
+    state = {
+        profilephoto: '',
+        hasimg: false
+    };
 
-  changeImg(e){
-    console.log(e.target);
-    console.log(e.target.files[0]);
-    
-    if (e.target.files && e.target.files[0]) {
-      let reader = new FileReader();
-      reader.onload = (event) => {
-          this.setState({profilephoto: event.target.result});
-      };
-      reader.readAsDataURL(e.target.files[0]);
-  }
+    changeImg(e) {
+        console.log(e.target);
+        console.log(e.target.files[0]);
 
-    this.setState({hasimg:true});
-    
-  }
+        if (e.target.files && e.target.files[0]) {
+            let reader = new FileReader();
+            reader.onload = event => {
+                this.setState({ profilephoto: event.target.result });
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
 
-  render() {
-    return (
-      <div className="background">
-        <MuiThemeProvider>
-          <div className="space">
+        this.setState({ hasimg: true });
+    }
 
-        </div>
-          <div className="rectangle">
-          <div className="space">
-
-          </div>
-            <h2 className="step-two">Step Two</h2>
-            <h2 className="profile-picture">PROFILE PICTURE</h2>
-            <div className="oval">
-              {
-                  !this.state.hasimg ? 
-                  <div className="oval-2">
-                    
-                  <div className="plus" >
-                  <div className="line"></div>
-                  <div className="line-copy"></div>
-                </div>
-                <input type="file" onChange={(e)=>{this.changeImg(e)}} style={inputimg}></input>
-                
-                </div>
-                :
-                <img src={this.state.profilephoto} className="oval-4" alt="Profile Photo"></img>
-                }
-             
+    render() {
+        return (
+            <div className="background">
+                <MuiThemeProvider>
+                    <div className="reg-rectangle">
+                        <br/>
+                        <h2 className="step-two-heading">Step Two</h2>
+                        <h2 className="pro-pic-heading">PROFILE PICTURE</h2>
+                        <br/>
+                        <div className="pro-pic-placeholder">
+                            {!this.state.hasimg ? (
+                                <div className="upload-circle">
+                                    <div className="plus-logo">
+                                        <div className="plus-horizontal" />
+                                        <div className="plus-vertical" />
+                                    </div>
+                                    <input
+                                        type="file"
+                                        onChange={e => {
+                                            this.changeImg(e);
+                                        }}
+                                        style={inputimg}
+                                    />
+                                </div>
+                            ) : (
+                                <img
+                                    src={this.state.profilephoto}
+                                    className="pro-pic-jpg"
+                                    alt="Profile"
+                                />
+                            )}
+                        </div>
+                        <br/>
+                        <div
+                            className="next-step-btn"
+                            onClick={event => this.handleClick(event)}>
+                            <h2 className="next-step-lbl">NEXT STEP</h2>
+                        </div>
+                        <br/>
+                        <h6 className="skip-for-now">Skip for now</h6>
+                    </div>
+                </MuiThemeProvider>
             </div>
-            <RaisedButton className="button" label="Next Step" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
-            <h6 className="skip-for-now">Skip for now</h6>
-         </div>
-        </MuiThemeProvider>
-      </div>
-    );
-  }
+        );
+    }
 }
-const style = {
-  margin: 15,
+
+const inputimg = {
+    opacity: 0,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%'
 };
-
-const inputimg ={
-  opacity: 0,
-  position: "absolute",
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  width: "100%"
-}
-
-
 
 export default Register2;
